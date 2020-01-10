d303b96c15f7683dc04d1b8ed9f941fb5dcf9ff8
Sophie Wattis <w1620496@student.southstaffs.ac.uk>
2020-01-10 15:10:00 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight() + 1,
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
